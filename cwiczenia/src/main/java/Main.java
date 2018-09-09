5b62bf68c45344227b57fee1a3f047650a95f590
Adam Smakulski <adam.smakulski@gmail.com>
2018-09-09 08:35:13 +0200

    interfejs i klasa anonimowa

--- cwiczenia/src/main/java/Main.java
+++ cwiczenia/src/main/java/Main.java
@@ -1,5 +1,24 @@
+import java.util.Collections;
+import java.util.Comparator;
+
 public class Main {
 
     public static void main(String[] args) {
+        carryOutWork(new MyTask());
+
+        carryOutWork(new Task() {
+            @Override
+            public void doWork() {
+                System.out.println("Hello from: " + this.getClass());
+            }
+        });
+    }
+
+    public static void carryOutWork(Task task) {
+        System.out.println("before");
+
+        task.doWork();
+
+        System.out.println("after");
     }
 }
---
+++ cwiczenia/src/main/java/MyTask.java
@@ -0,0 +1,7 @@
+public class MyTask implements Task {
+
+    @Override
+    public void doWork() {
+        System.out.println("Hello from: " + this.getClass());
+    }
+}
