7f91bbb00499f84960c4a643a27fc926f6401e42
Adam Smakulski <adam.smakulski@gmail.com>
2018-09-09 09:05:50 +0200

    Cwiczenie 2

--- cwiczenia/src/main/java/Main.java
+++ cwiczenia/src/main/java/Main.java
@@ -4,31 +4,37 @@ import java.util.Comparator;
 public class Main {
 
     public static void main(String[] args) {
-        carryOutWork(new MyTask());
+//        carryOutWork(new MyTask());
+//
+//        carryOutWork(new Task() {
+//            @Override
+//            public void doWork() {
+//                System.out.println("Hello from: " + this.getClass());
+//            }
+//        });
 
-        carryOutWork(new Task() {
+        carryOutWork(new MyRunnable());
+
+        carryOutWork(new Runnable() {
             @Override
-            public void doWork() {
-                System.out.println("Hello from: " + this.getClass());
+            public void run() {
+                System.out.println("hello from Runnable");
             }
         });
 
-    carryOutWork(
-            () -> System.out.println("Hello from lambda")
-    );
+        carryOutWork(() -> System.out.println("Hello from lambda"));
 
-    carryOutWork(() -> {
+        carryOutWork(() -> {
             System.out.println("Hello from lambda 2");
-            System.out.println("Hello again");
+            System.out.println("Hello again!!!");
         });
     }
 
-    public static void carryOutWork(Task task) {
+    public static void carryOutWork(Runnable task) {
         System.out.println("before");
 
-        task.doWork();
+        task.run();
 
         System.out.println("after");
     }
-
 }
--- cwiczenia/src/main/java/Task.java
+++ cwiczenia/src/main/java/Task.java
@@ -1,3 +1,4 @@
+@FunctionalInterface
 public interface Task {
     void doWork();
 }
